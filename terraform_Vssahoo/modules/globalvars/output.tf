c9d675bad69254586495204d379c58d0224ee2a4
EC2 Default User <ec2-user@ip-172-31-37-243.ec2.internal>
2023-06-08 05:26:39 +0000

    Tis is my first commit by vssahoo

---
+++ terraform_Vssahoo/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "vssahoo"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "vssahoo_Assignment1_"
+}
